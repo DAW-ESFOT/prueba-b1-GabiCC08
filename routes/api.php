376dd97d287086e8c25678486f314e3ca4a0b7a1
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2021-01-06 18:58:53 -0500

    Creación de rutas y controladores para movie, genre, user.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Movie;
+use App\Models\Genre;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('users', 'App\Http\Controllers\UserController@index');
+Route::get('users/{user}', 'App\Http\Controllers\UserController@show');
+Route::post('users', 'App\Http\Controllers\UserController@store');
+Route::put('users/{user}', 'App\Http\Controllers\UserController@update');
+Route::delete('users/{user}', 'App\Http\Controllers\UserController@delete');
+
+Route::get('movies', 'App\Http\Controllers\MovieController@index');
+Route::get('movies/{movie}', 'App\Http\Controllers\MovieController@show');
+Route::post('movies', 'App\Http\Controllers\MovieController@store');
+Route::put('movies/{movie}', 'App\Http\Controllers\MovieController@update');
+Route::delete('movies/{movie}', 'App\Http\Controllers\MovieController@delete');
+
+Route::get('genres', 'App\Http\Controllers\GenreController@index');
+Route::get('genres/{genre}', 'App\Http\Controllers\GenreController@show');
+Route::post('genres', 'App\Http\Controllers\GenreController@store');
+Route::put('genres/{genre}', 'App\Http\Controllers\GenreController@update');
+Route::delete('genres/{genre}', 'App\Http\Controllers\GenreController@delete');
