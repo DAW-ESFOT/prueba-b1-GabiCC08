237542587df3f9edc28481746765be9ba1deeff7
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2021-01-06 17:53:35 -0500

    Creación y configuración inicial del proyecto - Creación y migraciónde los modelos

---
+++ database/migrations/2021_01_06_173149_create_genres_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code',80);
+            $table->double('year');
+            $table->boolean('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genres');
+    }
+}
